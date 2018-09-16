13af8f9e9b84ff920950f66614af498fe49820c1
Alex Hand <alex.j.hand@gmail.com>
2018-09-15 19:02:22 -0500

    App.js set up

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ require('dotenv').config();
 const port = process.env.PORT || 4500;
 
 // Require Routers
+const indexRouter = require('./routes/index.router');
+const mailRouter = require('./routes/mail.router');
+
+
+// Middleware
+app.use(express.static('./client/build'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Use Routers
+app.use('/mail', mailRouter);
+app.use('/', indexRouter);
+
+// Listener
+app.listen(port, function() {
+    console.log('Listening on port ', port);
+});
+
+// Exports
+exports = module.exports = app;

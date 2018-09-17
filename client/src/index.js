3826c382f8a744a5488e7a6aec48aebc756d9133
Alex Hand <alex.j.hand@gmail.com>
2018-09-17 10:53:05 -0500

    Added hot swap

--- client/src/index.js
+++ client/src/index.js
@@ -6,3 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// Updates the page contents without performing a page refresh
+if (module.hot) {
+    module.hot.accept();
+}

8ad367a27b9e0617f05becabafd2032c5664e4d7
Alex Hand <alex.j.hand@gmail.com>
2018-09-26 10:48:46 -0500

    Built portfolio button functionality

--- client/src/components/LFCProjectsItem.js
+++ client/src/components/LFCProjectsItem.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-export default class ProjectsItem extends React.Component {
+export default class LFCProjectsItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentImg: 0
+        }
+        this.nextImage = this.nextImage.bind(this);
+        this.previousImage = this.previousImage.bind(this);
+    }
+
+    nextImage() {
+        this.setState({currentImg: this.state.currentImg === this.props.imgArray.length - 1 ? 0 : this.state.currentImg + 1});
+    }
+
+    previousImage() {
+        this.setState({currentImg: this.state.currentImg === 0 ? this.props.imgArray.length - 1 : this.state.currentImg - 1});
+    }
+    
     render() {
         return(
             <div className="projects__item">
-                <div className="card">
+                {/* <div className="card">
                     <div className="projects__item-display">
                         <input checked type="radio" className="projects__radio-input" id="lfcOne" name="lfc" />
                         <div className="projects__item-group" id="lfcHome">
@@ -29,14 +47,16 @@ export default class ProjectsItem extends React.Component {
                         <input type="radio" className="projects__radio-input" id="lfcThree" name="lfc" />
                         <div className="projects__item-group" id="lfcGoals">
                             <img className="projects__item-slide" src={require ("../images/lfc_scheduler/goals_Screenshot.png")} alt="Premier League Goal Leaders Table" />
-                                <label for="lfcTwo" className="projects__radio-label projects__item-left">
-                                    <div className="projects__item-cursor">&#10094;</div>
-                                </label>
-                                <label for="lfcOne" className="projects__radio-label projects__item-right">
-                                    <div className="projects__item-cursor">&#10095;</div>
-                                </label>
+                            <label for="lfcTwo" className="projects__radio-label projects__item-left">
+                                <div className="projects__item-cursor">&#10094;</div>
+                            </label>
+                            <label for="lfcOne" className="projects__radio-label projects__item-right">
+                                <div className="projects__item-cursor">&#10095;</div>
+                            </label>
                         </div>
                     </div>
+
+                        
 
                     <div className="cardContainer">
                         <h4>
@@ -55,6 +75,10 @@ export default class ProjectsItem extends React.Component {
                         </h4>
                         <p>React, Node, Express, Request, Cheerio, HTML5, and CSS3.</p>
                     </div>
+                </div> */}
+                <div className="test">
+                    Current Url: {this.props.imgArray[this.state.currentImg]}
+                    <button onClick={this.previousImage}>Previous</button><button onClick={this.nextImage}>Next</button>
                 </div>
             </div>
         )
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import ProjectsItem from '../components/ProjectsItem';
+import LFCProjectsItem from '../components/LFCProjectsItem';
 
 export default class Projects extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            lfcImages: [
+                "../images/lfc_scheduler/home_Screenshot.png",
+                "../images/lfc_scheduler/table_Screenshot.png",
+                "../images/lfc_scheduler/goals_Screenshot.png"
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="projects">
-                <ProjectsItem />
+                <LFCProjectsItem imgArray={this.state.lfcImages}/>
             </div>
         )
     }
